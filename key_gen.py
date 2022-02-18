8c02ae95da555e19376969290681225c94034e3e
skadiri94 <skadiri94@yahoo.com>
2022-02-18 21:13:47 +0000

    updated commit

--- key_gen.py
+++ key_gen.py
@@ -1,6 +1,38 @@
-import sys
-import paramiko
+# import sys
+# import paramiko
 
-key = paramiko.RSAKey.generate(4096)
-print(key.get_base64())  # print public key
-key.write_private_key(sys.stdout)  # print private key
+# key = paramiko.RSAKey.generate(4096)
+# print(key.get_base64())  # print public key
+# key.write_private_key(sys.stdout)  # print private key
+
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+def generate_key():
+
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    private_key_pass = b"your-password"
+
+    encrypted_pem_private_key = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.BestAvailableEncryption(
+            private_key_pass))
+
+    pem_public_key = private_key.public_key().public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+    # print(encrypted_pem_private_key)
+    
+
+
+    private_key_file = open("secrete-rsa.pem", "w")
+    private_key_file.write(encrypted_pem_private_key.decode())
+    private_key_file.close()
+
+    public_key_file = open("secrete-rsa.pub", "w")
+    public_key_file.write(pem_public_key.decode())
+    public_key_file.close()
+    
+    return encrypted_pem_private_key.decode(), pem_public_key.decode()
--- main.py
+++ main.py
@@ -11,12 +11,24 @@ from functools import wraps
 from camera import VideoCamera
 import threading
 import os
+from key_gen import generate_key
 
 pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
 
+secrete_keys = generate_key()
+private_key = secrete_keys[0]
+public_key = secrete_keys[1]
+private_key = private_key.replace('-----BEGIN ENCRYPTED PRIVATE KEY-----\n', '')
+private_key = private_key.replace('\n-----END ENCRYPTED PRIVATE KEY-----\n', '')
+public_key= public_key.replace('-----BEGIN PUBLIC KEY-----\n', '')
+public_key = public_key.replace('\n-----END PUBLIC KEY-----\n', '')
+public_key = public_key.replace('\n', '')
+public_key = public_key.replace(' ', '')
+
+print(public_key)
 # App Globals (do not edit)
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ebb9cf53302547c18c87ef5ded432a12'
+app.config['SECRET_KEY'] = private_key
 QRcode(app)
 
 def token_required(func):
@@ -52,12 +64,19 @@ def auth():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST": 
-        if request.form['username'] and request.form['password'] == '123456':
+    
+    if request.method == "POST":
+    #     first_set = set(public_key)
+    #     second_set = set(request.form['publickey'])
+    #     difference = first_set.symmetric_difference(second_set)
+    #     lk = str(request.form['publickey']).replace(' ','').strip()
+    #     lk = lk.replace('\n', '')
+        if request.form['publickey'] == public_key:
+            
             session['logged_in'] = True
-
+            
             token = jwt.encode({
-                'user': request.form['username'],
+                'user': request.form['publickey'],
                 # don't foget to wrap it in str function, otherwise it won't work [ i struggled with this one! ]
                 'expiration': str(datetime.utcnow() + timedelta(seconds=60))
             },
